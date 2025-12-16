3471bf53f35b0d828ab04bb2a81f50ab3d6bc54a
Samariddinn <urinovsalohiddin077@gmail.com>
2025-12-16 21:39:09 +0500

    Added service worker

--- script.js
+++ script.js
@@ -143,3 +143,4 @@ if('serviceWorker' in navigator) {
         navigator.serviceWorker.register('/sw.js').then((reg) => console.log("Service worker registered", reg)).catch((err) => console.log("ERROR", err));
     })
 }
+
---
+++ sw.js
@@ -0,0 +1,35 @@
+const CACHE_NAME = 'notes-app-cache-v1';
+const urlsToCache = [
+    '/',
+    '/index.html',
+    '/style.css',
+    '/script.js',
+    '/assets/icons/delete.svg',
+    '/assets/icons/edit.svg',
+]
+
+self.addEventListener('install', (event) => {
+    event.waitUntil(
+        caches.open(CACHE_NAME).then((cache) => cache.addAll(urlsToCache)).then(() => self.skipWaiting())
+    )
+})
+
+self.addEventListener('activate', (event) => {
+    event.waitUntil(
+        caches.keys().then((cacheNames) => {
+            return Promise.all(
+                cacheNames.map((cache) => {
+                    if (cache !== CACHE_NAME) return caches.delete(cache);
+                })
+            );
+        })
+    );
+    self.clients.claim();
+});
+
+self.addEventListener('fetch', (event) => {
+    event.respondWith(
+        caches.match(event.request)
+            .then((response) => response || fetch(event.request))
+    );
+});
